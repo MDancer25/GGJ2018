50926ce36cd868f75c521d03900e5b77086720c7
JFLPardal <jflpardal@gmail.com>
2018-01-27 09:47:35 +0000

    police
    chase

---
+++ Juan Forrest, Juan!/Assets/police/PoliceController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PoliceController : MonoBehaviour {
+
+	public bool followPassport;
+
+	public GameObject currentTarget;
+	GameObject[] listOfTargets;
+
+	int numberOfJuans;
+
+	// Use this for initialization
+	void Start () {
+		listOfTargets = GameObject.FindGameObjectsWithTag ("Player");
+		Assert.IsNotNull (listOfTargets);
+
+		ChooseTarget ();
+		numberOfJuans = listOfTargets.Length;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//TODO
+	//chooses who to follow
+	private void ChooseTarget()
+	{
+		int randIndex = Random.Range (0, numberOfJuans);
+		currentTarget = listOfTargets [randIndex];
+	}
+}
---
+++ Juan Forrest, Juan!/Assets/police/PoliceMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(PoliceController))]
+public class PoliceMovement : MonoBehaviour {
+
+	[SerializeField] float stopDistance = 1;
+
+	PoliceController policeController;
+	AICharacterControl aiController;
+	//public GameObject currentTarget;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		aiController = GetComponent<AICharacterControl> ();
+		policeController = GetComponent<PoliceController> ();
+
+	}
+
+	void Update ()
+	{
+		CheckDistanceToTarget ();
+	}
+
+
+	//TODO
+	//checks if it is too close to a player ->  attacks and eventually game over conditions?
+	private void CheckDistanceToTarget()
+	{
+		if (TooCloseToTheTarget()) //distance between police and the currently target player
+		{
+			aiController.SetTarget (this.transform);
+		}
+		else
+			aiController.SetTarget (policeController.currentTarget.transform);
+	}
+
+	private bool  TooCloseToTheTarget()
+	{
+		return Vector3.Distance (transform.position, policeController.currentTarget.transform.position) < stopDistance;
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere (transform.position, stopDistance);
+
+		if(policeController.currentTarget != null)
+			Gizmos.DrawLine (transform.position, policeController.currentTarget.transform.position);
+	}
+}
